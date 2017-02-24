61c517a2efbc014916d2598033bea457c3d9a8ba
Marc White <mwhite@overstock.com>
2017-02-24 14:40:11 -0700

    separated vendor files in build

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+
 const webpack = require('webpack');
 const path = require('path');
 
 const config = {
   context: path.resolve(__dirname, './src'),
   entry: {
-	  app: './index.js'
+	  app: './index.js',
+	  vendor: ['react', 'react-dom']
   },
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -38,6 +41,10 @@ const config = {
 	new ExtractTextPlugin('styles.css'),
     new webpack.optimize.UglifyJsPlugin(),
     new HtmlWebpackPlugin({template: './html/index.html'}),
+	new CommonsChunkPlugin({
+		name: 'vendor',
+		filename: 'vendor.bundle.js'
+	})
 ]
 };
 
